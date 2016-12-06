a396a791636afdd38321c1c3aca389c999c98e46
Ben <ben.costolo@gmail.com>
2016-12-05 19:45:40 -0500

    indent

--- game/game.js
+++ game/game.js
@@ -19,32 +19,32 @@ var refresh = setInterval(draw, 10);
 var coords = [0, 50, 100, 150, 200, 300, 350, 400, 450];
 
 var kanye = {
-	x: coords[Math.floor(Math.random() * coords.length)],
-	y: coords[Math.floor(Math.random() * coords.length)],
-	alive: true,
+  x: coords[Math.floor(Math.random() * coords.length)],
+  y: coords[Math.floor(Math.random() * coords.length)],
+  alive: true,
 };
 
 var mic = {
-	x: 250,
-	y: 250,
+  x: 250,
+  y: 250,
 };
 
 var player = {
-    x: coords[Math.floor(Math.random() * coords.length)],
-    y: coords[Math.floor(Math.random() * coords.length)],
-    micEquip: false,
-    moving: false,
+  x: coords[Math.floor(Math.random() * coords.length)],
+  y: coords[Math.floor(Math.random() * coords.length)],
+  micEquip: false,
+  moving: false,
 };
 
 //draws the images onto the html canvas
 function draw(){
-	ctx.clearRect(0, 0, a.width, a.height);
-	ctx.beginPath();
-	ctx.drawImage(jay, player.x, player.y, 50, 50);
-	ctx.drawImage(kanyeImg, kanye.x, kanye.y, 50, 50);
-	ctx.drawImage(microphone, mic.x, mic.y, 50, 50);
-	ctx.closePath();
-	ctx.fill();
+  ctx.clearRect(0, 0, a.width, a.height);
+  ctx.beginPath();
+  ctx.drawImage(jay, player.x, player.y, 50, 50);
+  ctx.drawImage(kanyeImg, kanye.x, kanye.y, 50, 50);
+  ctx.drawImage(microphone, mic.x, mic.y, 50, 50);
+  ctx.closePath();
+  ctx.fill();
 }
 
 //event listener for wasd input
@@ -52,125 +52,125 @@ window.addEventListener('keydown', playerMove, true);
 
 //moves the player
 function playerMove(evt){
-	switch (evt.keyCode) {
-		case 87:  /* w was pressed */
-			if (player.y - dy >= 0){ 
-			    player.y -= dy;
-			    player.moving = true;
-			    houseKeeping();			    
-			}
-		break;
-		case 83:  /* s was pressed */
-			if (player.y + dy < a.height){ 
-			    player.y += dy;
-			    player.moving = true;
-			    houseKeeping();			    
-			}
-		break;
-		case 65:  /* a was pressed */
-			if (player.x - dx >= 0){ 
-			    player.x -= dx;
-			    player.moving = true;
-			    houseKeeping(); 
-			}
-		break;
-		case 68:  /* d was pressed */
-			if (player.x + dx < a.width){ 
-				if(player.micEquip === true && player.x === 400){
-					break;
-				}
-		    player.x += dx;
-		    player.moving = true;
-		    houseKeeping();
-			}
-		break;
-	}
+  switch (evt.keyCode) {
+    case 87:  /* w was pressed */
+      if (player.y - dy >= 0){ 
+        player.y -= dy;
+        player.moving = true;
+        houseKeeping();			    
+      }
+      break;
+    case 83:  /* s was pressed */
+      if (player.y + dy < a.height){ 
+        player.y += dy;
+        player.moving = true;
+        houseKeeping();			    
+      }
+      break;
+    case 65:  /* a was pressed */
+      if (player.x - dx >= 0){ 
+        player.x -= dx;
+        player.moving = true;
+        houseKeeping(); 
+      }
+      break;
+    case 68:  /* d was pressed */
+      if (player.x + dx < a.width){ 
+        if(player.micEquip === true && player.x === 400){
+          break;
+        }
+        player.x += dx;
+        player.moving = true;
+        houseKeeping();
+      }
+      break;
+  }
 }
 
 //ai movement
 function aiMove() {
-	var ranNum = Math.floor((Math.random() * 4) + 1);
-	if (ranNum === 1){
-		if (kanye.y - dy >= 0){
-			kanye.y -= dy;
-		} else {
-			kanye.y = kanye.y;
-		}
-	} else if (ranNum === 2) {
-		if (kanye.y + dy < a.height) {
-			kanye.y += dy;
-		} else {
-			kanye.y = kanye.y;
-		}
-	} else if (ranNum === 3) {
-		if (kanye.x - dx >= 0) {
-			kanye.x -= dx;
-		} else {
-			kanye.x = kanye.x;
-		}
-	} else {
-		if (kanye.x + dx < a.width){
-			kanye.x += dx;
-		} else {
-			kanye.x = kanye.x;
-		}
-	}
+  var ranNum = Math.floor((Math.random() * 4) + 1);
+  if (ranNum === 1){
+    if (kanye.y - dy >= 0){
+      kanye.y -= dy;
+    } else {
+      kanye.y = kanye.y;
+    }
+  } else if (ranNum === 2) {
+    if (kanye.y + dy < a.height) {
+      kanye.y += dy;
+    } else {
+      kanye.y = kanye.y;
+    }
+  } else if (ranNum === 3) {
+    if (kanye.x - dx >= 0) {
+      kanye.x -= dx;
+    } else {
+      kanye.x = kanye.x;
+    }
+  } else {
+    if (kanye.x + dx < a.width){
+      kanye.x += dx;
+    } else {
+      kanye.x = kanye.x;
+    }
+  }
 }
 
 //regulates micEquip property on player, mic coords when equipped, whether kanye is alive, and 
 //win/lose conditions
 function houseKeeping() {
-	aiMove();
-	keyPressCount += 1;
-	if (keyPressCount === 1){
-		startTime = Date.now();
-	}
+  aiMove();
+  keyPressCount += 1;
+  if (keyPressCount === 1){
+    startTime = Date.now();
+  }
 
-	if (player.moving === true) {
-		console.log("player is moving x:" + player.x + " y:" + player.y);
-	}	
+  if (player.moving === true) {
+    console.log("player is moving x:" + player.x + " y:" + player.y);
+  }	
 
-	if (player.x === mic.x && player.y === mic.y){
-		player.micEquip = true;
-	}
+  if (player.x === mic.x && player.y === mic.y){
+    player.micEquip = true;
+  }
 
-	if (player.micEquip === false && kanye.x == mic.x && kanye.y == mic.y){
-		setTimeout(function(){
-		alert("kanye got the mic, you lose");
-		}, 15);
-		clearInterval(refresh);
-		rant.play();
-	}
+  if (player.micEquip === false && kanye.x == mic.x && kanye.y == mic.y){
+    setTimeout(function(){
+      alert("kanye got the mic, you lose");
+    }, 15);
+    clearInterval(refresh);
+    rant.play();
+  }
 
-	if (player.micEquip === true) {
-		mic.x = player.x + 50;
-		mic.y = player.y;
-		console.log("you got the mic");
-	}
+  if (player.micEquip === true) {
+    mic.x = player.x + 50;
+    mic.y = player.y;
+    console.log("you got the mic");
+  }
 
-	if(player.micEquip == true && mic.x == kanye.x && mic.y == kanye.y) {
-		endTime = Date.now();
-		var totalTime = ((endTime - startTime)/1000).toFixed(2);
-		var kps = (keyPressCount/totalTime).toFixed(2);
-		setTimeout(function(){
-		alert("check that trophy son, you won")}, 15);
-		kanye.alive = false;
-	}
+  if(player.micEquip == true && mic.x == kanye.x && mic.y == kanye.y) {
+    endTime = Date.now();
+    var totalTime = ((endTime - startTime)/1000).toFixed(2);
+    var kps = (keyPressCount/totalTime).toFixed(2);
+    setTimeout(function(){
+      alert("check that trophy son, you won")}, 15);
+    kanye.alive = false;
+  }
 
-	if (kanye.alive === false){
-		shoulder.play();
-		ctx2.drawImage(t, 500, 0, 500, 500);
-		ctx2.font="20px Helvetica";
-		ctx2.fillText(totalTime.toString() + " seconds", 515, 15, 500, 500);
-		ctx2.fillText(keyPressCount.toString() + " key presses", 515, 35, 500, 500);
-		ctx2.fillText(kps.toString() + " key presses per second", 515, 55, 500, 500);
-		console.log("you win!");
-		kanye.x = 100000;
-		kanye.y = 100000;
-		setTimeout(function(){
-		clearInterval(refresh);
-		}, 15);
-	}
+  if (kanye.alive === false){
+    shoulder.play();
+    ctx2.drawImage(t, 500, 0, 500, 500);
+    ctx2.font="20px Helvetica";
+    ctx2.fillText(totalTime.toString() + " seconds", 515, 15, 500, 500);
+    ctx2.fillText(keyPressCount.toString() + " key presses", 515, 35, 500, 500);
+    ctx2.fillText(kps.toString() + " key presses per second", 515, 55, 500, 500);
+    console.log("you win!");
+    kanye.x = 100000;
+    kanye.y = 100000;
+    setTimeout(function(){
+      clearInterval(refresh);
+    }, 15);
+  }
 }
 
 //loading images and sound file
