8c81bbc934f9ecb1c4fe0a0c234add3857e47124
Ben Costolo <ben.costolo@gmail.com>
2017-08-28 20:54:51 -0400

    standardify

--- seamus/app.js
+++ seamus/app.js
@@ -1,46 +1,47 @@
-var t = THREE;
-var scene = new t.Scene();
-var camera = new t.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+/* global THREE */
+var t = THREE
+var scene = new t.Scene()
+var camera = new t.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
 var seamuses = new t.MeshFaceMaterial([
-	new t.MeshBasicMaterial({
-		map: new t.TextureLoader().load('assets/pic1.jpeg')
-	}),
-	new t.MeshBasicMaterial({
-		map: new t.TextureLoader().load('assets/pic2.jpeg')
-	}),
-	new t.MeshBasicMaterial({
-		map: new t.TextureLoader().load('assets/pic3.jpeg')
-	}),
-	new t.MeshBasicMaterial({
-		map: new t.TextureLoader().load('assets/pic4.jpeg')
-	}),
-	new t.MeshBasicMaterial({
-		map: new t.TextureLoader().load('assets/pic5.jpeg')
-	}),
-	new t.MeshBasicMaterial({
-		map: new t.TextureLoader().load('assets/pic6.jpeg')
-	})
-]);
+  new t.MeshBasicMaterial({
+    map: new t.TextureLoader().load('assets/pic1.jpeg')
+  }),
+  new t.MeshBasicMaterial({
+    map: new t.TextureLoader().load('assets/pic2.jpeg')
+  }),
+  new t.MeshBasicMaterial({
+    map: new t.TextureLoader().load('assets/pic3.jpeg')
+  }),
+  new t.MeshBasicMaterial({
+    map: new t.TextureLoader().load('assets/pic4.jpeg')
+  }),
+  new t.MeshBasicMaterial({
+    map: new t.TextureLoader().load('assets/pic5.jpeg')
+  }),
+  new t.MeshBasicMaterial({
+    map: new t.TextureLoader().load('assets/pic6.jpeg')
+  })
+])
 
-var renderer = new t.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
+var renderer = new t.WebGLRenderer()
+renderer.setSize(window.innerWidth, window.innerHeight)
+document.body.appendChild(renderer.domElement)
 
-var geometry = new t.BoxGeometry(4, 4, 4);
+var geometry = new t.BoxGeometry(4, 4, 4)
 
-var cube = new t.Mesh(geometry, seamuses);
-scene.add(cube);
+var cube = new t.Mesh(geometry, seamuses)
+scene.add(cube)
 
-camera.position.z = 5;
+camera.position.z = 5
 
-var render = function() {
-	requestAnimationFrame(render);
+var render = function () {
+  requestAnimationFrame(render)
 
-	cube.rotation.x += 0.01;
-	cube.rotation.y += 0.01;
-	cube.rotation.z += 0.01;
+  cube.rotation.x += 0.01
+  cube.rotation.y += 0.01
+  cube.rotation.z += 0.01
 
-	renderer.render(scene, camera);
-};
+  renderer.render(scene, camera)
+}
 
-render();
+render()
